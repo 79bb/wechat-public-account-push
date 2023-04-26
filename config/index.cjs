f9b677856be8a950b4cb32d1f9dd68a83ad920cb
chenyang <115630646+79bb@users.noreply.github.com>
2023-04-26 22:20:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o9zew6gsn5Ewi3d3UdVbI9sBNle0',
+      id: 'o9zew6hnoc08VbulfmDX_Dhi8zaE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '16O2Ng4OH-0APlx1ees69fsiZtEDvD18vxIZt5R1QF8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
